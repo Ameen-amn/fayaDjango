7f6e4052808de3aa15c7f61c332594fe88305737
Ameen-amn <muhammedamn1@gmail.com>
2023-05-10 16:12:44 +0530

    date set

--- faya/views.py
+++ faya/views.py
@@ -10,8 +10,8 @@ def customer_list(request,format=None):
 #get all clients and serialize them and return Json file
 
     if request.method=='GET':
-        clients=Customer.objects.all()
-        serializer=CustomerSerializer(clients,many=True)
+        customer=Customer.objects.all()
+        serializer=CustomerSerializer(customer,many=True)
         return Response(serializer.data)
     
     if request.method=='POST':
@@ -25,16 +25,23 @@ def product_list(request,format=None):
 #get all product and serialize them and return Json file
 
     if request.method=='GET':
-        clients=Product.objects.all()
-        serializer=ProductSerializer(clients,many=True)
+        products=Product.objects.all()
+        serializer=ProductSerializer(products,many=True)
         return Response(serializer.data)
     
     if request.method=='POST':
         serializer=ProductSerializer(data=request.data)
+        expired=date.today()-timedelta(days=60)
+
         if serializer.is_valid():
+            registered_date = serializer.validated_data['registered_date']
+            expired = date.today() - timedelta(days=60)
+            if serializer.validated_data['registered_date'] <= expired.date():
+                serializer.validated_data['active'] = False
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 ##CUSTOMER     
 @api_view(['GET','PUT','DELETE'])
 def customer_detail(request,id,format=None):
@@ -73,12 +80,13 @@ def product_detail(request,id,format=None):
         serializer=ProductSerializer(product,data=request.data)
         expired=date.today()-timedelta(days=60)
 
-        if product.registered_date<=expired:
-            serializer.active=False
+        if product.registered_date<=expired and serializer.is_valid():
             
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
+            serializer.validated_data['active']=False
+            
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
     elif request.method=='DELETE':
